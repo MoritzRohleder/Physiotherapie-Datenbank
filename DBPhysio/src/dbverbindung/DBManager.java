6aad1d043d18ecf2e2f5b1ea76f082048fa21958
MoritzRohleder <moritz_rohleder@outlook.de>
2021-11-24 21:47:00 +0100

    änderung bei code together

--- DBPhysio/src/dbverbindung/DBManager.java
+++ DBPhysio/src/dbverbindung/DBManager.java
@@ -3,9 +3,10 @@ package src.dbverbindung;
 import java.sql.*;
 
 public class DBManager {
-    private static final String url="";
+    private static final String url="jdbc:oracle:thin:@172.22.112.100:1521:fbpool";
 
-    public static void showTable(){
-
+    public static String showTable(String table){
+        String sqlString = String.format("SELECT * FROM %s",table);
+        return "";
     }
 }
