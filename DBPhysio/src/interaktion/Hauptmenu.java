a87124aa57f15196d9fc3dc7361c3f4f484732f8
Crimson <60007253+CrimsonDynamic@users.noreply.github.com>
2021-11-25 12:55:44 +0100

    added tabellenauswahl

--- DBPhysio/src/interaktion/Hauptmenu.java
+++ DBPhysio/src/interaktion/Hauptmenu.java
@@ -12,6 +12,7 @@ public class Hauptmenu {
 			wahl = sc.nextInt();
 			switch (wahl){
 				case 1: // Anzeigen
+					tabellenauswahlmenue();
 					break;
 				case 2: // Einfügen
 					break;
@@ -37,5 +38,30 @@ public class Hauptmenu {
 		System.out.println("(4) Loeschen");
 		System.out.println("(5) Anzahl Tupel");
 		System.out.println("(6) Ende");
+	}
+	public static void tabellenauswahlmenue()
+	{
+
+		System.out.println("(1) Tabelle1");
+		System.out.println("(2) Tabelle2");
+		System.out.println("(3) Tabelle3");
+		System.out.println("(4) Tabelle4");
+		System.out.println("(5) Tabelle5");
+		System.out.println("(6) Ende");
+		wahl = sc.nextInt();
+		switch(wahl){
+			case 1: // Tabelle 1
+				break;
+			case 2: // Tabelle 2
+				break;
+			case 3: // Tabelle 3
+				break;
+			case 4: // Tabelle 4
+				break;
+			case 5: // Tabelle 5
+				break;
+			default:
+				break;
+		}
 	}
 }
